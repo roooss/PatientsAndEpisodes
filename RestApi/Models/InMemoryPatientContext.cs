0b5e543b0c9b3a298baf9f04f2aeac87495f63ff
Ross Nesbitt <ross.a.nesbitt@mail.com>
2016-04-03 15:55:34 +0100

    Added solution to the repo
    Added the solution to work on.

---
+++ RestApi/Models/InMemoryPatientContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using RestApi.Interfaces;
+
+namespace RestApi.Models
+{
+    public class InMemoryPatientContext : IDatabaseContext
+    {
+        private readonly InMemoryDbSet<Patient> _patients = new InMemoryDbSet<Patient>();
+        private readonly InMemoryDbSet<Episode> _episodes = new InMemoryDbSet<Episode>();
+
+        public IDbSet<Patient> Patients
+        {
+            get { return _patients; }
+        }
+
+        public IDbSet<Episode> Episodes
+        {
+            get { return _episodes; }
+        }
+    }
+}
